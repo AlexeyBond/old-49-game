e1433b6da50e122fb4506173667708ec82e36b6e
A.Bond <alexey.bond.94.55@gmail.com>
2021-10-03 13:40:20 +0600

    spinny things, more levels

--- test.gd
+++ test.gd
@@ -9,9 +9,19 @@ func _ready():
 	add_child(level_node)
 	add_to_group('level_manager')
 
+func change_level(level):
+	remove_child(level_node);
+	level_node = load(level).instance()
+	add_child(level_node)
+
 func next_level():
 	var nl = level_node.next_level;
 	if not nl: nl = first_level;
-	remove_child(level_node);
-	level_node = load(nl).instance()
-	add_child(level_node)
+	change_level(nl)
+
+func restart():
+	change_level(first_level);
+
+func _process(delta):
+	if Input.is_action_just_released("restart"):
+		restart()
